788b3aa9ca501accb5115e12d99325e913cea573
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-09-10 18:28:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function preventRefreshOnSubmit(){
 preventRefreshOnSubmit()
 
 function retrieveEmployeeInformation() {
-  return document.querySelector('input').value
+   document.querySelector('input').value
 }
 
 function addNewElementAsLi() {
